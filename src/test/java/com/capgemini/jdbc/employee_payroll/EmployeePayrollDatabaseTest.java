343d9b8aefbb39b37b4e617d10f0ef0cf4016cd9
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-11-01 17:12:28 +0530

    [Mukesh] add : EmployeePayrollDatabase class written

---
+++ src/test/java/com/capgemini/jdbc/employee_payroll/EmployeePayrollDatabaseTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.jdbc.employee_payroll;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EmployeePayrollDatabaseTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
